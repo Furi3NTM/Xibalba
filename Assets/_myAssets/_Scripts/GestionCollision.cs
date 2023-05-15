72f4b10c62bbe38f418c20d4c337600e962294f1
Furi3NTM <charles.varindupuis@hotmail.fr>
2023-05-15 09:59:12 -0400

    lot of nice change

---
+++ Assets/_myAssets/_Scripts/GestionCollision.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GestionCollision : MonoBehaviour
+{
+
+    
+    private bool _touche; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        _touche = false;  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
