c38867f0a0e819733e1eecfb9bb456305cb8916d
Kk0lt <lyesaid29@gmail.com>
2023-05-17 19:32:21 -0400

    Revert "player"
    This reverts commit e69abb8e9ac86bb5df247848a4bfa9771934b40c.

--- Assets/_myAssets/_Scripts/Player.cs
+++ Assets/_myAssets/_Scripts/Player.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,20 +6,7 @@ public class Player : MonoBehaviour
 {
 
     [SerializeField] private float _speed = 15f;
-    [SerializeField] private float _delaiMelee = 2f;
 
-    [SerializeField] private int _viesJoueur = 3;
-
-
-    private float _canFire = -1;
-
-    private Animator _anim;
-
-    private void Awake()
-    {
-        _anim = GetComponent<Animator>();
-
-    }
     // Start is called before the first frame update
     void Start()
     {
@@ -30,35 +16,11 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Move();
-
-        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
-        {
-            Melee();
-        }
-        else if (Input.GetKeyUp(KeyCode.Space))
-        {
-            _anim.SetBool("attaque_cac", false);
-        }
-    }
-
-    private void Melee()
-    {
-        _canFire = Time.time + _delaiMelee;
-        _anim.SetBool("attaque_cac", true);
-        Debug.Log("Melee animation triggered!");
-    }
-
-
-    private void Move()
-    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0f);
         transform.Translate(direction * Time.deltaTime * _speed);
+
+
     }
-
-
-
-
 }
