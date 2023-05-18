d33143004bbc8c49c2546e21ba0c1c40cebd540b
Furi3NTM <charles.varindupuis@hotmail.fr>
2023-05-17 21:08:08 -0400

    City almost completed

--- Assets/_myAssets/_Scripts/Enemy.cs
+++ Assets/_myAssets/_Scripts/Enemy.cs
@@ -4,26 +4,38 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] public float moveSpeed = 5f;
+    [SerializeField] public int maxHealth = 100; // Ajout de la variable de vie
+                     public int damageAmount = 50;
+
     public Transform player;
-   [SerializeField] public float moveSpeed = 5f;
+
     private Rigidbody2D rbEnemy;
     private Vector2 movementEnemy;
 
-
+    // Variable de vie actuelle
+    private int currentHealth;
 
     void Start()
     {
         rbEnemy = GetComponent<Rigidbody2D>();
+
+        // Initialisation de la vie actuelle en fonction de l'ennemi
+        if (gameObject.CompareTag("Thief"))
+        {
+            currentHealth = 50;
+        }
+        else if (gameObject.CompareTag("Knight"))
+        {
+            currentHealth = 100;
+        }
     }
 
-    
     void Update()
     {
         Vector3 direction = player.position - transform.position;
-
         direction.Normalize();
         movementEnemy = direction;
-
     }
 
     private void FixedUpdate()
@@ -35,4 +47,23 @@ public class Enemy : MonoBehaviour
     {
         rbEnemy.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
     }
+
+    // Méthode pour infliger des dégâts à l'ennemi
+    public void TakeDamage()
+    {
+        currentHealth -= damageAmount;
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    // Méthode appelée lorsque l'ennemi meurt
+    void Die()
+    {
+        // Ajoutez ici le code pour détruire l'ennemi, jouer une animation, etc.
+        Destroy(gameObject);
+    }
+
+
 }
