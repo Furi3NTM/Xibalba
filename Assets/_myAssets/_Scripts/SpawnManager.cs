ca6e84318c110f5e965c109ae079a4fe1fe5f51a
Furi3NTM <charles.varindupuis@hotmail.fr>
2023-05-20 22:18:28 -0400

    modification du spawn manager

--- Assets/_myAssets/_Scripts/SpawnManager.cs
+++ Assets/_myAssets/_Scripts/SpawnManager.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-
     [SerializeField] private GameObject prefabThief = default;
     [SerializeField] private GameObject prefabKnight = default;
     [SerializeField] private GameObject prefabPriest = default;
@@ -13,6 +12,9 @@ public class SpawnManager : MonoBehaviour
     public GameObject player;
 
     private bool stopSpawn = false;
+    private float initialDelay = 10.0f;
+    private float minDelay = 5f;
+    private float delayMultiplier = 0.01f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,45 +22,37 @@ public class SpawnManager : MonoBehaviour
         StartCoroutine(SpawnEnemyRoutine());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-
-    /* create a couroutine */
     IEnumerator SpawnEnemyRoutine()
     {
-        /* wait 3 second between first enemy */
-        yield return new WaitForSeconds(1.0f);
+        yield return new WaitForSeconds(initialDelay);
+
+        float currentDelay = initialDelay;
 
         while (stopSpawn == false)
         {
-            Vector3 theifSpawn = new Vector3(Random.Range((player.transform.position.x + 2 ) - 8f, player.transform.position.x + 8f), Random.Range(player.transform.position.y - 8f, player.transform.position.y + 8f), player.transform.position.z + Random.Range(-2f, 2f));
-            GameObject newTheif = Instantiate(prefabThief, theifSpawn, Quaternion.identity);
-            newTheif.transform.parent = enemyContainer.transform;
-            newTheif.SetActive(true);
+            SpawnEnemy(prefabThief);
+            SpawnEnemy(prefabKnight);
+            SpawnEnemy(prefabPriest);
 
-            Vector3 knightSpawn = new Vector3(Random.Range((player.transform.position.x + 2) - 10f, player.transform.position.x + 10f), Random.Range(player.transform.position.y - 10f, player.transform.position.y + 10f), player.transform.position.z + Random.Range(-2f, 2f));
-            GameObject newKnight = Instantiate(prefabKnight, knightSpawn, Quaternion.identity);
-            newKnight.transform.parent = enemyContainer.transform;
-            newKnight.SetActive(true);
+            yield return new WaitForSeconds(currentDelay);
 
-            Vector3 priestSpawn = new Vector3(Random.Range((player.transform.position.x + 2) - 12f, player.transform.position.x + 12f), Random.Range(player.transform.position.y - 12f, player.transform.position.y + 12f), player.transform.position.z + Random.Range(-2f, 2f));
-            GameObject newPriest = Instantiate(prefabPriest, priestSpawn, Quaternion.identity);
-            newPriest.transform.parent = enemyContainer.transform;
-            newPriest.SetActive(true);
-
-            yield return new WaitForSeconds(Random.Range(1.0f, 5.0f));
+            currentDelay = Mathf.Max(currentDelay * delayMultiplier, minDelay);
         }
+    }
 
+    void SpawnEnemy(GameObject enemyPrefab)
+    {
+        Vector3 enemySpawn = new Vector3(Random.Range((player.transform.position.x + 2) - 8f, player.transform.position.x + 8f),
+                                         Random.Range(player.transform.position.y - 8f, player.transform.position.y + 8f),
+                                         player.transform.position.z + Random.Range(-2f, 2f));
+
+        GameObject newEnemy = Instantiate(enemyPrefab, enemySpawn, Quaternion.identity);
+        newEnemy.transform.parent = enemyContainer.transform;
+        newEnemy.SetActive(true);
     }
 
     public void EndEnemySpawn()
     {
         stopSpawn = true;
     }
-
-
 }
