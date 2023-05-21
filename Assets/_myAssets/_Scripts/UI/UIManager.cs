8ee1819eaa18103ef6571b3263325b9b7428df2f
Kk0lt <lyesaid29@gmail.com>
2023-05-21 19:36:34 -0400

    pause menu

--- Assets/_myAssets/_Scripts/UI/UIManager.cs
+++ Assets/_myAssets/_Scripts/UI/UIManager.cs
@@ -2,28 +2,66 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
 
 
     [SerializeField] private TextMeshProUGUI _txtScore = default;
+    [SerializeField] private GameObject _pausePanel = default;
 
     private int _score =0;
+
+    private bool _pauseOn = false;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        _score = 0;
+        _pauseOn = false;
+        Time.timeScale = 1;
+
         UpdateScore();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MettreEnPause();
     }
 
-    //Gestion de score
+    //gestion de pause ============
+    private void MettreEnPause()
+    {
+        // Permet la gestion du panneau de pause (marche/arrêt)
+        if ((Input.GetKeyDown(KeyCode.Escape) && !_pauseOn))
+        {
+            _pausePanel.SetActive(true);
+            Time.timeScale = 0;
+            _pauseOn = true;
+        }
+        else if ((Input.GetKeyDown(KeyCode.Escape) && _pauseOn))
+        {
+            _pausePanel.SetActive(false);
+            Time.timeScale = 1;
+            _pauseOn = false;
+        }
+    }
+    // Méthode qui relance la partie après une pause
+    public void ResumeGame()
+    {
+        _pausePanel.SetActive(false);
+        Time.timeScale = 1;
+        _pauseOn = false;
+    }
+
+    public void ChargerDepart()
+    {
+        SceneManager.LoadScene(0);
+    }
+    //Gestion de score =====================
     public void AjouterScore(int points)
     {
         _score += points;
